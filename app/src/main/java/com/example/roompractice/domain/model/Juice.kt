ced29e366d1a0433b9ebbd1193651d5162e07446
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-08-23 15:48:24 +0530

    Add Juice model

---
+++ app/src/main/java/com/example/roompractice/domain/model/Juice.kt
@@ -0,0 +1,9 @@
+package com.example.roompractice.domain.model
+
+data class Juice(
+    val id: Long = 0L,
+    val name: String,
+    val description: String,
+    val color: String,
+    val rating: Int
+)
