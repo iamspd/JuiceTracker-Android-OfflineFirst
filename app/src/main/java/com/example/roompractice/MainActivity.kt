0138276e0b9bb13cdc62b1c2b6088053b7920b3b
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-08-28 21:27:32 +0530

    Add HomeScreen.kt

--- app/src/main/java/com/example/roompractice/MainActivity.kt
+++ app/src/main/java/com/example/roompractice/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.roompractice.ui.home.HomeScreen
 import com.example.roompractice.ui.theme.RoomPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RoomPracticeTheme {
+                HomeScreen()
             }
         }
     }
---
+++ app/src/main/java/com/example/roompractice/ui/home/HomeScreen.kt
@@ -0,0 +1,130 @@
+package com.example.roompractice.ui.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roompractice.R
+import com.example.roompractice.di.AppViewModelProvider
+import com.example.roompractice.ui.components.AppBottomSheet
+import com.example.roompractice.ui.home.components.FAB
+import com.example.roompractice.ui.home.components.JuiceList
+import com.example.roompractice.ui.home.components.SheetForm
+import com.example.roompractice.ui.home.components.SheetHeader
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    val homeViewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
+    val homeUiState by homeViewModel.homeUiState.collectAsStateWithLifecycle()
+    val currentJuice by homeViewModel.currentJuice.collectAsStateWithLifecycle()
+    val ratingInput by homeViewModel.ratingInput.collectAsStateWithLifecycle()
+
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = rememberStandardBottomSheetState(
+            initialValue = SheetValue.Hidden,
+            skipHiddenState = false
+        )
+    )
+
+    val scope = rememberCoroutineScope()
+    val sheetTitle = if (currentJuice?.id == 0L || currentJuice == null) {
+        stringResource(R.string.bottom_sheet_title_new_juice)
+    } else {
+        stringResource(R.string.bottom_sheet_title_update_juice)
+    }
+
+    LaunchedEffect(Unit) {
+        homeViewModel.dismissBottomSheet.collect {
+            scope.launch {
+                bottomSheetScaffoldState.bottomSheetState.hide()
+            }
+        }
+    }
+    LaunchedEffect(bottomSheetScaffoldState.bottomSheetState.isVisible) {
+        if (!bottomSheetScaffoldState.bottomSheetState.isVisible) {
+            homeViewModel.onSheetDismissed()
+        }
+    }
+
+    AppBottomSheet(
+        scaffoldState = bottomSheetScaffoldState,
+        sheetContent = {
+            SheetHeader(
+                title = sheetTitle,
+                modifier = Modifier.fillMaxWidth()
+            )
+            SheetForm(
+                juice = currentJuice,
+                onFormEvent = { homeViewModel.onFormEvent(it) },
+                ratingInput = ratingInput,
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+        mainContent = {
+            Scaffold(
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        title = { Text(stringResource(R.string.top_app_bar_title)) },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                },
+                floatingActionButton = {
+                    FAB(
+                        onClick = {
+                            homeViewModel.onAddNewJuice()
+                            scope.launch { bottomSheetScaffoldState.bottomSheetState.expand() }
+                        }
+                    )
+                }
+            ) { innerPadding ->
+                if (homeUiState.isJuiceListEmpty) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
+                        Text(
+                            text = stringResource(R.string.home_screen_juice_list_empty_text),
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                } else {
+                    JuiceList(
+                        juices = homeUiState.juices,
+                        onListEvent = { event ->
+                            homeViewModel.onListEvent(event)
+                            if (event is JuiceListEvent.JuiceSelected) {
+                                scope.launch { bottomSheetScaffoldState.bottomSheetState.expand() }
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    )
+                }
+            }
+        }
+    )
+}
